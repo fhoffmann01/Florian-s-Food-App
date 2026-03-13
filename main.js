388dcbcc8fa9e55bcf1e423e4bf666f9c83dddef
fhoffmann01 <flhoffmann01@users.noreply.github.com>
2026-03-13 23:54:05 +0100

    Render card with keys and values

--- main.js
+++ main.js
@@ -14,6 +14,16 @@ inputForm.addEventListener('submit', (e) => {
 
     const node = document.importNode(template.content, true);
 
+    node.querySelector(".ingredient-name").textContent = "Ei"
+    node.querySelector(".ingredient-amount").textContent = "100 g" 
+    node.querySelector(".calories").textContent = "156 kcal" 
+    node.querySelector(".carbohydrates").textContent = "1.1 g" 
+    node.querySelector(".thereof-sugars").textContent = "1.1 g" 
+    node.querySelector(".fats").textContent = "11 g" 
+    node.querySelector(".thereof-fattyacids").textContent = "0 g" 
+    node.querySelector(".proteins").textContent = "13 g" 
+    node.querySelector(".fibre").textContent = "0 g" 
+    node.querySelector(".salt").textContent = "0.3 g" 
    
     
     displayDataSection.appendChild(node);
