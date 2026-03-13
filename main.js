33c68e8bdd165db0e1a5f15fd27532020beb91b1
fhoffmann01 <flhoffmann01@users.noreply.github.com>
2026-03-13 21:58:54 +0100

    Get form input

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log("hello world!")
+const inputForm = document.getElementById("input-form")
+
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const inputFormData = new FormData(inputForm)
+    const formObj = Object.fromEntries(inputFormData)
+
+    console.log(formObj["name"])
+    console.log(formObj["amount"])
+})
+
