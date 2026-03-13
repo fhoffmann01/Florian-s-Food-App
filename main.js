7c1395ab12f662e7c19625e4404a39c89029d904
fhoffmann01 <flhoffmann01@users.noreply.github.com>
2026-03-13 23:31:41 +0100

    Render card with keys.

--- main.js
+++ main.js
@@ -6,7 +6,13 @@ inputForm.addEventListener('submit', (e) => {
     const inputFormData = new FormData(inputForm)
     const formObj = Object.fromEntries(inputFormData)
 
-    console.log(formObj["name"])
-    console.log(formObj["amount"])
+    console.log(formObj["name"]);
+    console.log(formObj["amount"]);
+
+    const displayDataSection = document.querySelector(".display-data-section");
+    const template = document.getElementById("ingredient-card");
+
+    const node = document.importNode(template.content, true);
+    displayDataSection.appendChild(node);
 })
 
