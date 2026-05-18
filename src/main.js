88ef55b5277ac92e0a2a735db746f50a1916f281
fhoffmann01 <flhoffmann01@users.noreply.github.com>
2026-05-18 23:27:41 +0200

    Add eventlisteners for error, success and upgradeneeded events to the open request

--- src/main.js
+++ src/main.js
@@ -65,5 +65,18 @@ const IDB = (function init() {
     let db = null;
     let objectStore = null;
     let DBOpenReq = indexedDB.open("FoodDB");
+    
+    DBOpenReq.addEventListener('error', err => {
+        console.log(err);
+    })
 
+    DBOpenReq.addEventListener('success', e => {
+        db = e.target.result;
+        console.log('success', db)
+    })
+
+    DBOpenReq.addEventListener('upgradeneeded', e => {
+        db = e.target.result;
+        console.log('upgrade', db)
+    })
 })()
